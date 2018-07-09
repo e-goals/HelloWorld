e444f82835247ccbcd07598cda8742947f5ac163
ez-goals <alanturing@yeah.net>
2018-07-09 23:49:02 +0800

    Version 2.0

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World, This is the first program.");
+		System.out.println("Hello World, This is the version 2.0");
 	}
 
 }
